a470ca66896a2b3f7b05786028753d183d525015
Taposh-Das <taposhexp@gmail.com>
2021-06-17 12:23:43 -0500

    Global State In Last commit, we had the our tasks object as a comment in Tasks.js But we want to make it globla state. So we will put it in the App.js that way it becomes a global state. Benifits of making it a global state is, we can pass it down to any components we want as props. ~

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import { useState } from "react"
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 function App() {
+  // now the task is part of our App.js component state
+  const [tasks, setTasks] = useState ([
+    {
+        id: 1,
+        text: "Doctors Appointment",
+        day: 'Feb 5th at 2:30',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: "Meeting at School",
+        day: 'Feb 6th at 1:30',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: "Food Shopping",
+        day: 'Feb 5th at 2:30',
+        reminder: false,
+    },
+  ])
+
   return (
     <div className="container">
       <Header title={"My first react app"}/>
-      <Tasks />
+      // now we can take our tasks and pass them in component as prop
+      <Tasks tasks={tasks} />
     </div>
   );
 }
