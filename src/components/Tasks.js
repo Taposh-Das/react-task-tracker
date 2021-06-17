560d636a20899d5a6ff201f9ba2e7347a6ab2c3e
Taposh-Das <taposhexp@gmail.com>
2021-06-17 11:30:36 -0500

    State and useState() The task object map/array we created in last commit, it was separate from our component. We want this to be part of our state So we pass the tasks object to a react hook called "useState()".
    Now tasks object is part of our component state
    
    In our return function we use the tasks to access the tasks object.
    Since tasks object is immutable we don't use tasks.push() to update or add anything new to tasks object.
    
    We use the "setTasks" we defined when we called "useState()" hook.

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,25 +1,27 @@
-const tasks = [
-    {
-        id: 1,
-        text: "Doctors Appointment",
-        day: 'Feb 5th at 2:30',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: "Meeting at School",
-        day: 'Feb 6th at 1:30',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: "Food Shopping",
-        day: 'Feb 5th at 2:30',
-        reminder: false,
-    },
-]
+import { useState } from "react"
 
 const Tasks = () => {
+    const [tasks, setTasks] = useState ([
+        {
+            id: 1,
+            text: "Doctors Appointment",
+            day: 'Feb 5th at 2:30',
+            reminder: true,
+        },
+        {
+            id: 2,
+            text: "Meeting at School",
+            day: 'Feb 6th at 1:30',
+            reminder: true,
+        },
+        {
+            id: 3,
+            text: "Food Shopping",
+            day: 'Feb 5th at 2:30',
+            reminder: false,
+        },
+    ])
+
     return (
         <>
         {tasks.map((task) => (
