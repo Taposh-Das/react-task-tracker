0540f86ee52812f2747b67085cb1fb628c1707e5
Taposh-Das <taposhexp@gmail.com>
2021-06-18 23:38:58 -0500

    JSON Server
    We first isntall Json server using the command "npm -i json-server"
    
    Ther we add the json server in the package file and add the port and watch properties
    
    Now we can run the server using command npm run server in our dev terminal.
    
    Then I transfer the tasks array data from App.js to the db.json file as json object
    
    This server acts as our backend wtih rest api.
    
    Going to localhost:5000/tasks returns all the tasks we added in db.json like an a rest api

--- src/App.js
+++ src/App.js
@@ -7,26 +7,7 @@ function App() {
 
   const [showAddTask, setShowAddTask] = useState(false)
   // now the task is part of our App.js component state
-  const [tasks, setTasks] = useState ([
-    {
-        id: 1,
-        text: "Doctors Appointment",
-        day: 'Feb 5th at 2:30',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: "Meeting at School",
-        day: 'Feb 6th at 1:30',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: "Food Shopping",
-        day: 'Feb 5th at 2:30',
-        reminder: false,
-    },
-  ])
+  const [tasks, setTasks] = useState ([])
 
   // Add Task
   const addTask = (task) => {
