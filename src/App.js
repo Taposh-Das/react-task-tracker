d2b95f013b677af8cc2ef467dbb30698c33bff5f
Taposh-Das <taposhexp@gmail.com>
2021-06-19 09:59:20 -0500

    Routing, footer and about First we install react router dom using this command "npm i react-router-dom" Once installed, it will show on the package.json
    Then create two component Footer and About js.
    
    We add routes so that we can go from about to home page
    
    Also instead of <a> tag for link we use Link because clicking <a> links makes the page reload where is Link tags are fast without any reload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { useState, useEffect } from "react"
+import { BrowserRouter as Router, Route } from "react-router-dom"
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+import About from "./components/About";
+import { dom } from "aria-query";
 
 function App() {
 
   const [showAddTask, setShowAddTask] = useState(false)
   // now the task is part of our App.js component state
-  const [tasks, setTasks] = useState ([])
+  const [tasks, setTasks] = useState([])
 
   useEffect(() => {
-    const getTasks = async() => {
+    const getTasks = async () => {
       const tasksFromServer = await fetchtasks()
       setTasks(tasksFromServer)
     }
@@ -59,23 +63,24 @@ function App() {
   }
 
   // Delete Task
-  const deleteTask = async(id) => {
+  const deleteTask = async (id) => {
     // this line is to delete task from json server
-    await fetch(`http://localhost:5000/tasks/${id}`,{
+    await fetch(`http://localhost:5000/tasks/${id}`, {
       method: 'DELETE'
     })
 
     /*for each delete of task, we filter out the id on which the red cross
     icon was clicked. and only display the rest of the tasks*/
-    setTasks(tasks.filter((task) => task.id!==id))
+    setTasks(tasks.filter((task) => task.id !== id))
   }
 
   // Toggle Reminder
   const toggleReminder = async (id) => {
-    const taskToToggle =  await fetchtask(id)
-    const updatedTask = {...taskToToggle, reminder: !taskToToggle.reminder }
+    const taskToToggle = await fetchtask(id)
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
 
-    const res = await fetch(`http://localhost:5000/tasks/${id}`,{
+    // we are updating one task i.e. making reminder true or false
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
       method: 'PUT',
       headers: {
         'Content-type': 'application/json'
@@ -85,21 +90,29 @@ function App() {
 
     const data = await res.json()
 
-    setTasks(tasks.map((task) => task.id === id ? 
-    { ...task, reminder: data.reminder } : task))
+    setTasks(tasks.map((task) => task.id === id ?
+      { ...task, reminder: data.reminder } : task))
   }
 
   return (
     //For Tasks, now we can take our tasks object and pass it in component as prop
-    <div className="container">
-      <Header title={"My first react app"} 
-      onAdd={() => setShowAddTask (!showAddTask)} 
-      showAdd={showAddTask} />
-      {showAddTask && <AddTask onAdd={addTask}/>}
-      {tasks.length>0 ? <Tasks tasks={tasks} 
-      onDelete={deleteTask} onToggle={toggleReminder} />:
-      ("No Task to show")}
-    </div>
+    <Router>
+      <div className="container">
+        <Header title={"My first react app"}
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask} />
+        <Route path='/' exact render={(props) => (
+          <>
+            {showAddTask && <AddTask onAdd={addTask} />}
+            {tasks.length > 0 ? <Tasks tasks={tasks}
+              onDelete={deleteTask} onToggle={toggleReminder} /> :
+              ("No Task to show")}
+          </>
+        )} />
+        <Route path='/about' component={About} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
 import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
 import Button from './Button'
 
 const Header = ({title, onAdd, showAdd}) => {
+    // this gives us the current route location's
+    const location = useLocation()
+
     return (
         <header className ='header'>
             <h1>{title}</h1>
-            <Button color={showAdd ? 'Red' : 'Green'} 
+           {location.pathname ==='/' && 
+           (<Button color={showAdd ? 'Red' : 'Green'} 
             text={showAdd ? 'Close' : 'Add'} 
             onClick={onAdd} />
+            )}
         </header>
     )
 }
